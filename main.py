d6eb5900e153c85cc8248b0ee6eb3af0e67d36af
Stella Waithera <csstlw@gmail.com>
2022-04-28 20:20:39 +0000

    Explanation on binary search

--- main.py
+++ main.py
@@ -1,2 +1,4 @@
 #This is an algorithm that reduces the size of the input data in each size.The number of operations reduces as the input size increases
 #I will implement the binary search 
+#To perform a binary search in an array we repeatedly divide the search interval in half.
+#We begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty
