f6fcffff1195a88ce3a1f5d25517764e85333795
Stella Waithera <csstlw@gmail.com>
2022-04-28 20:59:25 +0000

    Add an arrray to perform a binary search

--- main.py
+++ main.py
@@ -3,3 +3,29 @@
 #To perform a binary search in an array we repeatedly divide the search interval in half.
 #We begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty
 #https://www.geeksforgeeks.org/complexity-analysis-of-binary-search/
+
+def binarySearch(array, arraylength, startValue, searchValue):
+
+  if startValue >= arraylength:
+    mid = arraylength + (startValue -     arraylength) // 2
+    #if element is at the middle
+    if array[mid] == searchValue:
+      return mid
+    #If element is smaler than mid it is in the left subarray
+    elif array[mid] > searchValue:
+      return binarySearch(array, arraylength, mid-1, searchValue)
+    #else the element is in the righ sub arra
+    else:
+      return binarySearch(array, mid+1, startValue, searchValue)
+  else:
+    #element not in array
+    return -1
+
+array = [45,29,61,21,40,31,82,90,30]
+searchValue = 90
+
+result = binarySearch(array, 0, len(array)-1, searchValue)
+if result != -1:
+  print("Element is at index %d" % result)
+else:
+  print("Element is not present in array")
