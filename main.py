bbc3ae9e538c936dd380d99bade2d317316a54a5
Stella Waithera <csstlw@gmail.com>
2022-04-28 20:22:44 +0000

    link to unrestand how log n is derived in a binary search algorithm

--- main.py
+++ main.py
@@ -2,3 +2,4 @@
 #I will implement the binary search 
 #To perform a binary search in an array we repeatedly divide the search interval in half.
 #We begin with an interval covering the whole array. If the value of the search key is less than the item in the middle of the interval, narrow the interval to the lower half. Otherwise, narrow it to the upper half. Repeatedly check until the value is found or the interval is empty
+#https://www.geeksforgeeks.org/complexity-analysis-of-binary-search/
